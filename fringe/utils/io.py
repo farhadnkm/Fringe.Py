bbce95b8ff243a64fa61a20b5d0ee9b1b76b4ae0
Farhad Niknam <farhad.niknam.em@gmail.com>
2021-02-26 14:34:01 +0330

    Minor issues fixed.

--- fringe/utils/io.py
+++ fringe/utils/io.py
@@ -1,53 +1,54 @@
 import numpy as np
 from skimage import io
 
+
 # Import / export / convert_type methods.
 
 
 def import_image(path, preprocessor=None):
-	"""
-	:param path: Directory of the image
-	:param preprocessor: An arbitrary class having a function 'process' which is performed on the images on import
-	:return: a numpy array containing the image
-	"""
-	img = io.imread(path)
+    """
+    :param path: Directory of the image
+    :param preprocessor: An arbitrary class having a function 'process' which is performed on the images on import
+    :return: a numpy array containing the image
+    """
+    img = io.imread(path)
 
-	print("Image imported from:", path)
-	if preprocessor is not None:
-		if hasattr(preprocessor, '__iter__'):
-			for pp in preprocessor:
-				img = pp.process(img=img)
-		else:
-			img = preprocessor.process(img=img)
+    print("Image imported from:", path)
+    if preprocessor is not None:
+        if hasattr(preprocessor, '__iter__'):
+            for pp in preprocessor:
+                img = pp.process(img=img)
+        else:
+            img = preprocessor.process(img=img)
 
-	return img
+    return img
 
 
 def import_image_seq(paths, preprocessor=None):
-	"""
-	:param paths: Directories of the images
-	:param preprocessor: An arbitrary class having a function 'process' which is performed on the images on import
-	:return: A list of images as numpy arrays
-	"""
-	imgs = []
-	for path in paths:
-		img = import_image(path, preprocessor)
-		imgs.append(img)
-	return imgs
+    """
+    :param paths: Directories of the images
+    :param preprocessor: An arbitrary class having a function 'process' which is performed on the images on import
+    :return: A list of images as numpy arrays
+    """
+    imgs = []
+    for path in paths:
+        img = import_image(path, preprocessor)
+        imgs.append(img)
+    return imgs
 
 
 def export_image(img, path, dtype='uint16'):
-	assert dtype in ['uint8', 'uint16']
-	_img = img.copy()
-	_img[_img > 1] = 1
-	_img[_img < 0] = 0
+    assert dtype in ['uint8', 'uint16']
+    _img = img.copy()
+    _img[_img > 1] = 1
+    _img[_img < 0] = 0
 
-	if dtype == 'uint8':
-		_img *= 2 ** 8 - 1
-		_img = np.uint8(_img)
-	elif dtype == 'uint16':
-		_img *= 2 ** 16 - 1
-		_img = np.uint16(_img)
+    if dtype == 'uint8':
+        _img *= 2 ** 8 - 1
+        _img = np.uint8(_img)
+    elif dtype == 'uint16':
+        _img *= 2 ** 16 - 1
+        _img = np.uint16(_img)
 
-	io.imsave(path, _img)
-	print("Image exported to:", path)
+    io.imsave(path, _img)
+    print("Image exported to:", path)
--- fringe/utils/modifiers.py
+++ fringe/utils/modifiers.py
@@ -2,92 +2,93 @@ import numpy as np
 from skimage import color
 import tensorflow as tf
 
+
 # Modifiers are classes having a 'process()' function. These functions could be passed as a parameter
 # to import_image methods in DataManager and are called on each image on import
 
 
 class ImageToArray:
-	def __init__(self, bit_depth=16, channel='gray', crop_window=None, dtype='float32'):
-		"""
-		A preprocess class to convert the input image to a valid array for further processing.
-		:param bit_depth: bit_depth of the input image.
-		:param channel: Preferred channel of image for the processing: could be 'gray', 'r', 'g', 'b', 'avg_rgb'.
-		:param crop_window: [x, y, width, height] specifies the crop window. If is none, the actual size is considered.
-		:param dtype: Data type of the output array.
-		"""
-		self.bd = bit_depth
-		assert channel in ['gray', 'r', 'g', 'b', 'rgb']
-		self.chan = channel
-		self.crop = crop_window
-		self.dtype = dtype
+    def __init__(self, bit_depth=16, channel='gray', crop_window=None, dtype='float32'):
+        """
+        A preprocess class to convert the input image to a valid array for further processing.
+        :param bit_depth: bit_depth of the input image.
+        :param channel: Preferred channel of image for the processing: could be 'gray', 'r', 'g', 'b', 'avg_rgb'.
+        :param crop_window: [x, y, width, height] specifies the crop window. If is none, the actual size is considered.
+        :param dtype: Data type of the output array.
+        """
+        self.bd = bit_depth
+        assert channel in ['gray', 'r', 'g', 'b', 'rgb']
+        self.chan = channel
+        self.crop = crop_window
+        self.dtype = dtype
 
-	def process(self, img):
-		if len(np.shape(img)) == 2:
-			if self.chan in ['r', 'g', 'b', 'rgb']:
-				raise AssertionError('The image is grayscale but your expecting an RGB image.')
+    def process(self, img):
+        if len(np.shape(img)) == 2:
+            if self.chan in ['r', 'g', 'b', 'rgb']:
+                raise AssertionError('The image is grayscale but your expecting an RGB image.')
 
-		_img = img.astype(self.dtype)
-		_img /= 2 ** self.bd
+        _img = img.astype(self.dtype)
+        _img /= 2 ** self.bd
 
-		if self.chan == 'gray':
-			if len(np.shape(_img)) == 2:
-				pass
-			elif len(np.shape(_img)) == 3:
-				_img = color.rgb2gray(_img)
-			else:
-				raise ValueError('Input array is not an image or its type is not supported.')
-		elif self.chan == 'r':
-			_img = _img[:, :, 0]
-		elif self.chan == 'g':
-			_img = _img[:, :, 1]
-		elif self.chan == 'b':
-			_img = _img[:, :, 2]
-		elif self.chan == 'rgb':
-			pass
+        if self.chan == 'gray':
+            if len(np.shape(_img)) == 2:
+                pass
+            elif len(np.shape(_img)) == 3:
+                _img = color.rgb2gray(_img)
+            else:
+                raise ValueError('Input array is not an image or its type is not supported.')
+        elif self.chan == 'r':
+            _img = _img[:, :, 0]
+        elif self.chan == 'g':
+            _img = _img[:, :, 1]
+        elif self.chan == 'b':
+            _img = _img[:, :, 2]
+        elif self.chan == 'rgb':
+            pass
 
-		if self.crop is not None:
-			x, y, w, h = self.crop
-			_img = _img[y:y + h, x:x + w]
+        if self.crop is not None:
+            x, y, w, h = self.crop
+            _img = _img[y:y + h, x:x + w]
 
-		return _img
+        return _img
 
 
 class PreprocessHologram:
-	def __init__(self, background=None):
-		"""
-		A preprocess class to convert the input image to a hologram for further processing and reconstruction.
-		:param background: The background image of the hologram.
-		"""
-		self.bg = background
-		self.bg[self.bg == 0] = 1e-8
+    def __init__(self, background=None):
+        """
+        A preprocess class to convert the input image to a hologram for further processing and reconstruction.
+        :param background: The background image of the hologram.
+        """
+        self.bg = background
+        self.bg[self.bg <= 1e-8] = 1e-8
 
-	def process(self, img):
-		_img = np.sqrt(img)
-		if self.bg is not None:  # Normalize
-			_img /= self.bg
-		minh = np.min(_img)
-		_img -= minh
-		_img /= 1 - minh
-		return _img
+    def process(self, img):
+        _img = np.copy(img)
+        if self.bg is not None:  # Normalize
+            _img /= self.bg
+        minh = np.min(_img)
+        _img -= minh
+        _img /= 1 - minh
+        return _img
 
 
 class ConvertToTensor:
-	def __init__(self, dtype='float32'):
-		"""
-		:param dtype: Data type of the resulting tensor
-		"""
-		assert dtype in ['float32', 'float64', 'complex64', 'complex128']
-		self.dtype = dtype
+    def __init__(self, dtype='float32'):
+        """
+        :param dtype: Data type of the resulting tensor
+        """
+        assert dtype in ['float32', 'float64', 'complex64', 'complex128']
+        self.dtype = dtype
 
-	def process(self, img):
-		if self.dtype == self.dtype:
-			t = tf.complex(real=tf.convert_to_tensor(img, dtype='float32'),
-						   imag=tf.zeros_like(img, dtype='float32'))
-		elif self.dtype == 'complex128':
-			t = tf.complex(real=tf.convert_to_tensor(img, dtype='float64'),
-						   imag=tf.zeros_like(img, dtype='float64'))
-		elif self.dtype in ['float32', 'float64']:
-			t = tf.convert_to_tensor(img, dtype=self.dtype)
-		else:
-			raise TypeError(str(self.dtype) + ' is not supported')
-		return t
+    def process(self, img):
+        if self.dtype == 'complex64':
+            t = tf.complex(real=tf.convert_to_tensor(img, dtype='float32'),
+                           imag=tf.zeros_like(img, dtype='float32'))
+        elif self.dtype == 'complex128':
+            t = tf.complex(real=tf.convert_to_tensor(img, dtype='float64'),
+                           imag=tf.zeros_like(img, dtype='float64'))
+        elif self.dtype in ['float32', 'float64']:
+            t = tf.convert_to_tensor(img, dtype=self.dtype)
+        else:
+            raise TypeError(str(self.dtype) + ' is not supported')
+        return t
